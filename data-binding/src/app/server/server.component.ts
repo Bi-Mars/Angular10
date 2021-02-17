3db89ce17f25ff97724a49601293fd7b82f6c458
Bimarsh Sharma <bimarshsharma101@gmail.com>
2021-02-17 12:40:23 -0500

    String Interpolationa and Property Binding

---
+++ data-binding/src/app/server/server.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css']
+})
+export class ServerComponent implements OnInit {
+
+  allowNewServer = false;
+
+  constructor() {
+    setTimeout(()=>{
+      this.allowNewServer = true;
+    },2000)
+  }
+
+  ngOnInit(): void {
+  }
+
+  serverId: number = 10;
+  serverStatus: string = "offline";
+
+  getServerStatus() {
+    return this.serverStatus;
+  }
+
+
+}

77dd8378c51c9fb65fbc82d678d6f417d0178713
Bimarsh Sharma <bimarshsharma101@gmail.com>
2021-02-23 14:30:46 -0500

    Created skeleton for shopping and recipe projects: All componenets and subcomponents using ng g c componentPath/componentName --skipTests true

---
+++ shopping-and-recipe/src/app/header/header.component.ts
@@ -0,0 +1,11 @@
+//import
+
+import { Component } from "@angular/core";
+
+//component
+@Component({
+    selector: "app-header",
+    templateUrl: "./header.component.html"
+})
+//export
+export class HeaderComponent{}
